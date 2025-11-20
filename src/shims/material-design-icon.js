db9461b6b1a9ff90e8c8c0d1f38387ebe269c763
Asma Signate <contactagadget@gmail.com>
2025-11-20 18:30:23 +0000

    Create material-design-icon.js shim for icons
    Add a shim for @react-native-vector-icons/material-design-icons.

---
+++ src/shims/material-design-icon.js
@@ -0,0 +1,4 @@
+// Shim minimal pour module @react-native-vector-icons/material-design-icons attendu par react-native-paper.
+// Si tu veux des icônes réelles sur le web, utilise @expo/vector-icons ou configure correctement react-native-vector-icons pour le web.
+
+module.exports = {};
